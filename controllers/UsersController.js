d445c59ff636a81682ae1751b6f21952a634df65
ThamerAyachi <thamerjak3@gmail.com>
2023-12-19 12:07:57 +0100

    task 3

---
+++ controllers/UsersController.js
@@ -0,0 +1,34 @@
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+const UsersController = {
+  async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    try {
+      const user = await dbClient.getUserByEmail(email);
+      if (user) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      const hashedPwd = sha1(password);
+      const newUser = await dbClient.createUser(email, hashedPwd);
+
+      res.status(201).json({ email: newUser.email, id: newUser._id });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+    return null;
+  },
+};
+
+module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 // Require controllers
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 // Endpoints
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
